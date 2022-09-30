9927740a4787014517c1506556f1b2caa9d5433e
3nt3 <gott@3nt3.de>
2022-09-30 23:26:35 +0200

    blink two leds
    Signed-off-by: 3nt3 <gott@3nt3.de>

--- firmware/src/main.rs
+++ firmware/src/main.rs
@@ -12,19 +12,62 @@ use embedded_hal::digital::v2::OutputPin;
 use esp_idf_hal::delay::FreeRtos;
 use esp_idf_hal::gpio::*;
 use esp_idf_hal::peripherals::Peripherals;
+use esp_idf_sys::EspError;
+
+struct RGB {
+    r: u8,
+    g: u8,
+    b: u8
+}
+
+struct RGBPins {
+    r_pin: OutputPin,
+    b_pin: OutputPin,
+    g_pin: OutputPin
+}
+
+impl RGBPins {
+    fn new(&mut self,  r_pin: &mut OutputPin,  g_pin:&mut OutputPin,  b_pin: &mut OutputPin) {
+        self.r_pin = r_pin;
+        self.g_pin = g_pin;
+        self.b_pin = b_pin;
+    }
+
+    fn set_rgb(&mut self, &rgb: RGB) {
+    }
+}
 
 fn main() -> anyhow::Result<()> {
     esp_idf_sys::link_patches();
 
     let peripherals = Peripherals::take().unwrap();
-    let mut led = peripherals.pins.gpio26.into_output()?;
+
+    let mut r = peripherals.pins.gpio26.into_output()?;
+    // AHHH this can only be an input pin like physically
+    // let mut g = peripherals.pins.gpio34.into_output()?;
+    let mut b = peripherals.pins.gpio13.into_output()?;
+
+    let mut rgb_pins 
 
     loop {
-        led.set_high()?;
-        // we are sleeping here to make sure the watchdog isn't triggered
-        thread::sleep(time::Duration::from_millis(1000));
-
-        led.set_low()?;
-        thread::sleep(time::Duration::from_millis(1000))
+        println!("blinking red");
+        blink_pin_thrice(&mut r)?;
+        // println!("blinking green");
+        // blink_pin_thrice(&mut g);
+        println!("blinking blue");
+        blink_pin_thrice(&mut b)?;
     }
 }
+
+fn blink_pin_thrice(pin: &mut dyn OutputPin<Error = EspError>) -> Result<(), EspError> {
+    for _ in 1..3 {
+        pin.set_high()?;
+        thread::sleep(time::Duration::from_millis(1000));
+
+        pin.set_low()?;
+        thread::sleep(time::Duration::from_millis(1000));
+    }
+    Ok(())
+}
+
+fn set_rgb()
